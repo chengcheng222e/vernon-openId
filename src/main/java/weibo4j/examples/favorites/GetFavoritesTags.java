ec476009a48f7f6dde278e9e65a5c4677a8ee403
陈袁 <chengcheng222e@sina.com>
2014-05-29 17:04:50 +0800

    Initial commit

---
+++ src/main/java/weibo4j/examples/favorites/GetFavoritesTags.java
@@ -0,0 +1,26 @@
+package weibo4j.examples.favorites;
+
+import weibo4j.Favorite;
+import weibo4j.examples.oauth2.Log;
+import weibo4j.model.FavoritesTag;
+import weibo4j.model.WeiboException;
+
+import java.util.List;
+
+public class GetFavoritesTags {
+
+	public static void main(String[] args) {
+		String access_token = args[0];
+		Favorite fm = new Favorite();
+		fm.client.setToken(access_token);
+		try {
+			List<FavoritesTag> favors = fm.getFavoritesTags();
+			for(FavoritesTag s : favors){
+				Log.logInfo(s.toString());
+			}
+		} catch (WeiboException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
